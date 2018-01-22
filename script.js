73f57aa2952355eebff5e6698cbd21646b720aef
Aditya Verma <adi004.zz@gmail.com>
2018-01-22 23:55:56 +0530

    Added some AI logic

--- script.js
+++ script.js
@@ -1,18 +1,55 @@
 $(document).ready(function(){
-    console.log($(".corner").length);
-    let x = Math.floor(Math.random()*($(".corner").length));
-    // $(".corner")[x].animate("animated bounce").text("X")
-    // $(".corner")[x].text("X").animate({ marginTop: "80px" }, 1500 )
-    //            .animate({ marginTop: "40px" }, 800 );
-    // console.log($(".corner")[1]);
-    // let  y = $(".corner")[1]
-    // y.text("HELLO")
-    // console.log(x);
-    // console.log($(".corner:nth-of-type(" + x+1 + ")"), x);
-    $(".corner:eq(" + x + ")").text("X").animate({ marginTop: "80px" }, 1500 )
-               .animate({ marginTop: "40px" }, 800 );
-    // console.log($(".corner:eq(0)"))
-    // console.log($(".corner:eq(1)"))
-    // console.log($(".corner:eq(2)"))
-    // console.log($(".corner:eq(3)"))
+    // console.log($(".corner").length);
+
+    
+   
+    // $(".corner:eq(" + x + ")").text("X").effect( "bounce", "slow" );
+
+    // $("td").on("click", function(){
+    //     // alert("test");
+    //     $(this).text("O").effect("bounce", "slow");
+    //     console.log($(this).attr("id"));
+    // });
+    let turn = 1;
+    let whoGoesFirst = Math.floor(Math.random() * 2); // Chooses who goes first
+    
+    function AILogic(){
+        if (whoGoesFirst){
+            console.log("Players goes first");
+    
+    
+        }
+        else {
+            console.log("AI goes first");
+            let x = Math.floor(Math.random()*($(".corner").length));
+            if (turn == 1){
+                $(".corner:eq(" + x + ")").text("X").effect( "bounce", "slow" );
+            }
+            else if (turn == 2){
+                alert("2nd turn ai")
+                let x = Math.floor(Math.random()*($(".corner").length));
+                $(".corner:eq(" + x + ")").text("X").effect( "bounce", "slow" );
+            }
+        }
+    }
+    AILogic();
+
+    $("td").on("click", function(){
+        if (whoGoesFirst == 0){
+            if (turn == 1){
+                // let x = Math.floor(Math.random()*($(".corner").length));
+               
+                $(this).text("O").effect("bounce", "slow");
+                console.log($(this).attr("id"));
+                turn++;
+            }
+            else if (turn == 2){
+                let x = Math.floor(Math.random()*($(".corner").length));
+                $(this).text("O").effect("bounce", "slow");
+                alert("works");
+                turn++;
+                AILogic();
+            }
+        }
+    });
 });
